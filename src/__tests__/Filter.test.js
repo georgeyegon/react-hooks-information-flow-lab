fcda390077ebaa537339000fb846bd71e3c4e23f
George Yegon <georgeyegon67@gmail.com>
2024-11-04 14:27:45 +0300

    initial commit

--- src/__tests__/Filter.test.js
+++ src/__tests__/Filter.test.js
@@ -1,18 +1,19 @@
+// src/__tests__/Filter.test.js
 import "@testing-library/jest-dom";
 import { render, screen, fireEvent } from "@testing-library/react";
 import Filter from "../components/Filter";
 
 test("displays the <select> element", () => {
-  render(<Filter />);
-  expect(screen.queryByRole("combobox")).toBeInTheDocument();
+  const handleCategoryChange = jest.fn();
+  render(<Filter selectedCategory="All" onCategoryChange={handleCategoryChange} />);
+
+  expect(screen.getByRole("combobox")).toBeInTheDocument();
 });
 
-test("calls the onCategoryChange callback prop when the <select> is changed", () => {
-  const onCategoryChange = jest.fn();
-  render(<Filter onCategoryChange={onCategoryChange} />);
+test("calls the onCategoryChange when the category is changed", () => {
+  const handleCategoryChange = jest.fn();
+  render(<Filter selectedCategory="All" onCategoryChange={handleCategoryChange} />);
 
-  fireEvent.change(screen.queryByRole("combobox"), {
-    target: { value: "Dairy" },
-  });
-  expect(onCategoryChange).toHaveBeenCalled();
+  fireEvent.change(screen.getByRole("combobox"), { target: { value: "Dairy" } });
+  expect(handleCategoryChange).toHaveBeenCalled();
 });
--- src/__tests__/Header.test.js
+++ src/__tests__/Header.test.js
@@ -1,16 +1,19 @@
+// src/__tests__/Header.test.js
 import "@testing-library/jest-dom";
 import { render, screen, fireEvent } from "@testing-library/react";
 import Header from "../components/Header";
 
 test("displays the toggle button", () => {
-  render(<Header />);
-  expect(screen.queryByText(/ Mode/)).toBeInTheDocument();
+  const onDarkModeClick = jest.fn();
+  render(<Header onDarkModeClick={onDarkModeClick} isDarkMode={false} />);
+
+  expect(screen.getByText(/Mode/)).toBeInTheDocument();
 });
 
 test("calls the onDarkModeClick callback prop when the button is clicked", () => {
   const onDarkModeClick = jest.fn();
-  render(<Header onDarkModeClick={onDarkModeClick} />);
+  render(<Header onDarkModeClick={onDarkModeClick} isDarkMode={false} />);
 
-  fireEvent.click(screen.queryByText(/ Mode/));
+  fireEvent.click(screen.getByText(/Mode/));
   expect(onDarkModeClick).toHaveBeenCalled();
 });
